b257180a29c0f8b2ab1efbd1d3620537f2d02ca7
Choon Gee <cgee.code@gmail.com>
2022-08-31 00:10:36 +0800

    Add HomeScreen Special Offers section

--- consumer-app/src/components/SpecialOffer/index.tsx
+++ consumer-app/src/components/SpecialOffer/index.tsx
@@ -1,10 +1,33 @@
-import { View, Text } from "react-native";
+import { TouchableOpacity, Image } from "react-native";
 
-const SpecialOffer = () => {
+type Props = {
+  imageURL: string;
+  navigationTarget: string;
+  navigationTargetId: number;
+};
+
+const SpecialOffer = ({
+  imageURL,
+  navigationTarget,
+  navigationTargetId
+}: Props) => {
   return (
-    <View>
-      <Text>SpecialOffer</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        /* navigation code here */
+      }}
+      className="w-full"
+    >
+      <Image
+        source={require("../../../assets/speacial-offers-01.png")}
+        style={{
+          width: "100%",
+          height: undefined,
+          borderRadius: 36
+        }}
+        className="aspect-[2/1]"
+      />
+    </TouchableOpacity>
   );
 };
 
--- consumer-app/src/screens/HomeScreen/index.tsx
+++ consumer-app/src/screens/HomeScreen/index.tsx
@@ -4,20 +4,28 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "./Header";
 import SearchBar from "../../components/SearchBar";
 import SectionHeader from "../../components/SectionHeader";
+import SpecialOffer from "../../components/SpecialOffer";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView className="flex-1 bg-white font-regular">
       <Header />
-      <View className="mx-5 my-2">
+      <View className="mx-6 my-3">
         <SearchBar />
       </View>
-      <View className="mx-5 my-2">
+      <View className="mx-6 my-3">
         <SectionHeader
           title="Special Offers"
           navigationTarget="SpecialOffers"
           navigationText="See All"
         />
+        <View className="mt-6 shadow-lg shadow-slate-300">
+          <SpecialOffer
+            imageURL="image-url"
+            navigationTarget="MenuItemDetails"
+            navigationTargetId={123}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );

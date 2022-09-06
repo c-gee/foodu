ab9f8192759fb717a2a3da1f7cb4f14475051458
Choon Gee <cgee.code@gmail.com>
2022-09-06 16:29:13 +0800

    Add custom Splash screen

--- consumer-app/App.tsx
+++ consumer-app/App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import {
   useFonts,
   Urbanist_400Regular,
@@ -12,7 +14,10 @@ import {
 import { RootStackNavigation } from "./src/navigation";
 import SearchContextProvider from "./src/contexts/SearchContext";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   let [fontsLoaded] = useFonts({
     Urbanist_400Regular,
     Urbanist_500Medium,
@@ -20,12 +25,36 @@ export default function App() {
     Urbanist_700Bold
   });
 
-  if (!fontsLoaded) {
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  useEffect(() => {
+    async function prepare() {
+      if (!fontsLoaded) return;
+      // add other resource loading code
+
+      // await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      setAppIsReady(true);
+    }
+
+    prepare();
+  }, [fontsLoaded]);
+
+  if (!appIsReady) {
     return null;
   }
 
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
       <NavigationContainer>
         <SearchContextProvider>
           <RootStackNavigation />

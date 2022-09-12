2e4cb465b9f6c7e593fcedaa0328850c9deb000c
Choon Gee <cgee.code@gmail.com>
2022-09-12 16:07:55 +0800

    Add signUp and signIn APIs

--- api/src/graphql/modules/user/resolvers.ts
+++ api/src/graphql/modules/user/resolvers.ts
@@ -7,6 +7,9 @@ const resolvers: Resolvers = {
       const user = await prisma.user.findUnique({
         where: {
           email: args.email
+        },
+        include: {
+          socialProfiles: true
         }
       });
 
@@ -14,7 +17,7 @@ const resolvers: Resolvers = {
     }
   },
   Mutation: {
-    async createUser(_, args) {
+    async signUp(_, args) {
       const user = await prisma.user.create({
         data: {
           email: args.email,
@@ -23,7 +26,94 @@ const resolvers: Resolvers = {
           gender: args.gender,
           dateOfBirth: args.dateOfBirth,
           areaCode: args.areaCode,
+          phone: args.phone,
+          picture: args.picture
+        },
+        include: {
+          socialProfiles: true
+        }
+      });
+
+      return user;
+    },
+
+    async signInByProvider(_, args) {
+      return await prisma.$transaction(async (prisma) => {
+        let user;
+
+        const socialProfile = await prisma.socialProfile.upsert({
+          where: {
+            provider_sub: { provider: args.provider, sub: args.sub }
+          },
+          create: {
+            sub: args.sub,
+            provider: args.provider,
+            name: args.name || null,
+            email: args.email || null,
+            picture: args.picture || null,
+            userId: null
+          },
+          update: {
+            name: args.name || null,
+            email: args.email || null,
+            picture: args.picture || null
+          }
+        });
+
+        if (socialProfile.userId === null) {
+          const createUser = await prisma.user.create({
+            data: {
+              socialProfiles: {
+                create: {
+                  sub: args.sub,
+                  provider: args.provider,
+                  name: args.name || null,
+                  email: args.email || null,
+                  picture: args.picture || null
+                }
+              }
+            }
+          });
+
+          await prisma.socialProfile.update({
+            where: {
+              provider_sub: { provider: args.provider, sub: args.sub }
+            },
+            data: {
+              userId: createUser.id
+            }
+          });
+
+          user = await prisma.user.findUnique({
+            where: {
+              id: createUser.id
+            },
+            include: {
+              socialProfiles: true
+            }
+          });
+        } else {
+          user = await prisma.user.findUnique({
+            where: {
+              id: socialProfile.userId
+            },
+            include: {
+              socialProfiles: true
+            }
+          });
+        }
+
+        return user;
+      });
+    },
+
+    async signInByPhone(_, args) {
+      const user = await prisma.user.findUnique({
+        where: {
           phone: args.phone
+        },
+        include: {
+          socialProfiles: true
         }
       });
 
